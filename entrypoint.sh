ab7786ce32d0178b297d0083aedf6d45d137c5d2
heapops <heapops@gmail.com>
2019-06-12 00:03:04 +0800

    add docker-compose
    Signed-off-by: heapops <heapops@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@
 
 file=/etc/v2ray/config.json
 if [[ -f $file ]]; then
-    if [[ "$V2RAY_SERVER" && "$V2RAY_PORT" && "$V2RAY_ID" ]]; then
+    if [[ "$V2RAY_ID" ]]; then
         cat $file | sed "s/__V2RAY_SERVER__/$V2RAY_SERVER/" | sed "s/__V2RAY_PORT__/$V2RAY_PORT/" | sed "s/__V2RAY_ID__/$V2RAY_ID/" > /config.json
         v2ray -config=/config.json
     else
